c3d94ca765ec0a76c1c405a15d877fa1f1516592
Николай Кондалов <kondalovnikolay@gmail.com>
2022-01-03 17:55:23 +0500

    Добавлены имена игроков

--- SkillBoxGame/Form1.cs
+++ SkillBoxGame/Form1.cs
@@ -13,30 +13,50 @@ namespace SkillBoxGame
     public partial class Form1 : Form
     {
         bool turn = true;
+
         int turn_count = 0;
+
         static String player1, player2;
 
         public Form1()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Выход из игры. Кнопка "Exit"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ExitGame(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
+        /// <summary>
+        /// Метод, который отвечает за имена вводимые пользователем в Forms2
+        /// </summary>
+        /// <param name="n1">playeer 1</param>
+        /// <param name="n2">playeer 2</param>
         public static void SetPlayersName(String n1, String n2)
         {
             player1 = n1;
             player2 = n2;
         }
 
+        /// <summary>
+        /// Информация об авторе. Кнопка "Help". Да, кнопка help - автор так решил!!!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AboutMe(object sender, EventArgs e)
         {
             MessageBox.Show("Игра создана исключетельно в учебных целях!\n Разработчик: Кондалов Николай Николаевич", "Справка");
         }
 
+        /// <summary>
+        /// Метод, который отвечает за новую игру. Кнопка "New Game"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void NewGame(object sender, EventArgs e)
         {
             turn = true;
@@ -54,10 +74,13 @@ namespace SkillBoxGame
 
             }
         }
-
+        /// <summary>
+        /// Метод, который делает проверку игроков
+        /// </summary>
         private void CheckForWinner()
         {
             bool three_is_a_winner = false;
+
             //горизонтальные проверки
             if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
             {
@@ -99,9 +122,7 @@ namespace SkillBoxGame
             if (three_is_a_winner)
             {
                 DisabledButton();
-
-                String winner = "";
-
+                string winner;
                 if (turn)
                 {
                     //winner = "O";
@@ -127,6 +148,9 @@ namespace SkillBoxGame
                 }
             }
         }
+        /// <summary>
+        /// Метод, который отключает кнопки если игрок выйграл или была ничья
+        /// </summary>
         private void DisabledButton()
         {
             try
@@ -143,6 +167,11 @@ namespace SkillBoxGame
             }
         }
 
+        /// <summary>
+        /// Метод, который подсказывает ход(имя) игрока
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button_enter(object sender, EventArgs e)
         {
             Button b = (Button)sender;
@@ -150,14 +179,18 @@ namespace SkillBoxGame
             {
                 if (turn)
 
-                    b.Text = "X";
-                    //b.Text = "ход\n" + player1;
+                    //b.Text = "X";
+                    b.Text = "ход\n" + player1;
                 else
-                    b.Text = "O";
-                    //b.Text = "ход\n" + player2;
+                    //b.Text = "O";
+                    b.Text = "ход\n" + player2;
             }
         }
-
+        /// <summary>
+        /// Метод, который очищает поле если курсор мышки находится за пределами кнопок
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button_leave(object sender, EventArgs e)
         {
             Button b = (Button)sender;
@@ -166,7 +199,11 @@ namespace SkillBoxGame
                 b.Text = ""; // Если мышка за пределами кнопки, то поле будет снова пустым
             }
         }
-
+        /// <summary>
+        /// Сброс очков
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ResetCount(object sender, EventArgs e)
         {
             o_win_count.Text = "0";
@@ -182,6 +219,11 @@ namespace SkillBoxGame
             NameTwo.Text = player2;
         }
 
+        /// <summary>
+        /// Метод, который назначает кнопки согласно ходу игрока
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button_click(object sender, EventArgs e)
         {
             Button b = (Button)sender;
